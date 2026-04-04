c1a04b51862941bf0c758516717bcc8f6f9258cd
BILAL KHAIDOUME <bkhaidoume@gmail.com>
2026-04-05 00:27:34 +0100

    filled data.js with games data

--- src/data.js
+++ src/data.js
@@ -4,7 +4,32 @@ export const games = [
         title : "League of legends",
         price : 34.99,
         category: "RPG",
-        logo : "public/assets/sologo.png",
+        logo : "./public/assets/sologo.png",
         image : "public/assets/lol.png",
-    }
-]
+    },
+    {
+        id : 2 ,
+        title : "Valorant",
+        price : 24.99,
+        category: "RPG",
+        logo : "./public/assets/valorantlogo.png",
+        image : "public/assets/valorant.png",
+    },
+    {
+        id : 3 ,
+        title : "Teamfight Tactics",
+        price : 14.99,
+        category: "RPG",
+        logo : "./public/assets/Teamfightlogo.png",
+        image : "public/assets/Teamfight.png",
+    },
+    {
+        id : 4 ,
+        title : "Legends of Runeterra",
+        price : 14.99,
+        category: "RPG",
+        logo : "./public/assets/Runeterralogo.png",
+        image : "public/assets/Runeterra.png",
+    },
+
+];

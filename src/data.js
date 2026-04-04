d5604bd504da46b9e360dccebdcc115e27e4d693
BILAL KHAIDOUME <bkhaidoume@gmail.com>
2026-04-04 23:42:28 +0100

    created data.js

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const games = [
+    {
+        id : 1 ,
+        title : "League of legends",
+        price : 34.99,
+        category: "RPG",
+        logo : "public/assets/sologo.png",
+        image : "public/assets/lol.png",
+    }
+]
